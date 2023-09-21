7f48d9cb0bb48ee605aa864c26a645c41e0a730e
Dave <davearonmwan@gmail.com>
2023-09-21 01:27:36 +0100

    feat: add text footer to the gallery page

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -12,9 +12,21 @@ const GalleryPage = () => {
   }, [setImages]);
 
   return (
-    <main className=" mx-auto w-auto max-w-5xl px-4 pt-8">
+    <main className=" mx-auto w-auto max-w-5xl px-4 py-8">
       <GalleryHeader />
       <GalleryBody />
+      <p className="mt-8 text-center">
+        Built by{" "}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.github.com/d-a-ve/"
+          className="text-primary-500 transition duration-200 hover:text-primary-700"
+        >
+          Dave
+        </a>{" "}
+        for HNG X Zuri Stage 3 Task.
+      </p>
     </main>
   );
 };
